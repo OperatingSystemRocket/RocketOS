3482f6a5ce31b8cd5b4b34c48a32d6194838c403
LincolnSand <sandlincoln@gmail.com>
2021-02-10 21:46:47 -0800

    Just making the tests pass again as I finished testing buildkite

--- test/Test-cstring.c
+++ test/Test-cstring.c
@@ -5,7 +5,7 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void test_cstrlen(void) {
-    TEST_ASSERT_EQUAL_INT(4, cstrlen("hello"));
+    TEST_ASSERT_EQUAL_INT(5, cstrlen("hello"));
     TEST_ASSERT_EQUAL_INT(0, cstrlen(""));
     TEST_ASSERT_EQUAL_INT(3, cstrlen("foo"));
     TEST_ASSERT_EQUAL_INT(1, cstrlen("4"));
