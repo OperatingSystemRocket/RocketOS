7753805b0dca7543515a9ca035ec0b49aeafa3d3
LincolnSand <sandlincoln@gmail.com>
2021-02-12 11:28:56 -0800

    Wrote the assert_void(x) macro for use in void functions since void is not a type.

--- src/cassert.h
+++ src/cassert.h
@@ -2,11 +2,20 @@
 
 #include "vga_driver.h"
 
-#define assert(x, failure)   do { \
-                                if(!(x)) { \
-                                    terminal_write_color("Condition Failed: ", VGA_COLOR_RED); \
-                                    terminal_write_color(#x, VGA_COLOR_RED);                   \
-                                    terminal_write_color("\n", VGA_COLOR_RED); \
-                                    return failure; \
-                                } \
-                            } while(0)
+#define assert(x, return_code)  do { \
+                                    if(!(x)) { \
+                                        terminal_write_color("Condition Failed: ", VGA_COLOR_RED); \
+                                        terminal_write_color(#x, VGA_COLOR_RED);                   \
+                                        terminal_write_color("\n", VGA_COLOR_RED); \
+                                        return return_code; \
+                                    } \
+                                } while(0)
+
+#define assert_void(x)  do { \
+                            if(!(x)) { \
+                                terminal_write_color("Condition Failed: ", VGA_COLOR_RED); \
+                                terminal_write_color(#x, VGA_COLOR_RED);                   \
+                                terminal_write_color("\n", VGA_COLOR_RED); \
+                                return; \
+                            } \
+                        } while(0)
--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,4 @@
-//@todo implement scrolling when the screen fills up and newline (especially this second one)
+//@todo implement scrolling when the screen fills up
 
 #include "vga_driver.h"
 #include "cassert.h"
@@ -12,5 +12,4 @@ void kernel_main(void)
 	terminal_writestring("Hello, kernel World!\n");
     terminal_write_color("Hello, kernel World!\n", VGA_COLOR_RED);
     terminal_writestring("Second line\nThird!\n");
-    assert(1 == 2, 0);
 }
