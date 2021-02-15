f378cc86f0f16e6078c531a921e842c82e7d2624
dexter <dexterwest@gmail.com>
2021-02-15 14:29:04 -0800

    implented cerrno.h
    I made an cerrno.c file that contains the actual errno variable. cerrno.h
    is just the extern declaration and error value macros.
    
    Note: this isn't thread safe yet

---
+++ src/cerrno.c
@@ -0,0 +1,4 @@
+//
+// Created by dexter on 2/15/21.
+//
+
---
+++ src/cerrno.h
@@ -0,0 +1,8 @@
+//
+// Created by dexter on 2/15/21.
+//
+
+#ifndef ROCKETOS_CERRNO_H
+#define ROCKETOS_CERRNO_H
+
+#endif // ROCKETOS_CERRNO_H
