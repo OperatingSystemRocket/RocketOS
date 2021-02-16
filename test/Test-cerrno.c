d7fcf29292652e2a208878901bfbde3602437d20
LincolnSand <sandlincoln@gmail.com>
2021-02-15 16:16:34 -0800

    Fixed an issue with the empty test

--- test/Test-cerrno.c
+++ test/Test-cerrno.c
@@ -1,16 +1,12 @@
 #include "unity.h"
 
 
-void setUp(void) {
-    terminal_initialize_test(&buffer[0]); //so that printing doesn't seg fault
-}
+void setUp(void) {}
 void tearDown(void) {}
 
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_cstrlen);
-    RUN_TEST(test_cstrcmp);
     UNITY_END();
     return 0;
 }
