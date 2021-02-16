afd2dfd4888be34de2055328f7ed5b7fc8a14236
LincolnSand <sandlincoln@gmail.com>
2021-02-15 16:15:40 -0800

    Added an empty test file for cerrno

--- test/Test-cerrno.c
+++ test/Test-cerrno.c
@@ -1,4 +1,16 @@
-//
-// Created by dexter on 2/15/21.
-//
+#include "unity.h"
 
+
+void setUp(void) {
+    terminal_initialize_test(&buffer[0]); //so that printing doesn't seg fault
+}
+void tearDown(void) {}
+
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_cstrlen);
+    RUN_TEST(test_cstrcmp);
+    UNITY_END();
+    return 0;
+}
