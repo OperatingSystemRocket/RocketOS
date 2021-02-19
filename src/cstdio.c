6c64f5794d08834bc57eba43912518d4ca8852df
LincolnSand <sandlincoln@gmail.com>
2021-02-19 09:36:09 -0800

    Fixed a mistype

--- src/cstdio.c
+++ src/cstdio.c
@@ -1,7 +1,7 @@
 #include "cstdio.h"
 
 int cprintf(const char *const format, ...) {
-    for(uint32_t i = 0u; i < strlen(format); ++i) {
+    for(uint32_t i = 0u; i < cstrlen(format); ++i) {
 
     }
 }
