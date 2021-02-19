94f0d5b2a24eccb00914eaf6ac68a1b212f77c88
LincolnSand <sandlincoln@gmail.com>
2021-02-19 09:29:26 -0800

    Wrote cint_to_string

--- src/cstdio.h
+++ src/cstdio.h
@@ -1,3 +1,3 @@
 #pragma once
 
-
+int cprintf(const char *format, ...);
--- src/cstring.h
+++ src/cstring.h
@@ -7,3 +7,5 @@
 size_t cstrlen(const char* str);
 
 int cstrcmp(const char* lhs, const char* rhs);
+
+void cint_to_string(ssize_t input, char* string_ret, size_t ret_size);
