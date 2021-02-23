5f11cde2db8c5520cb6c858034f0bfce48d00041
LincolnSand <sandlincoln@gmail.com>
2021-02-23 11:27:45 -0800

    Wrote a test for kint_to_string and fixed a few minor bugs

--- test/Test-kstring.c
+++ test/Test-kstring.c
@@ -1,12 +1,25 @@
 #include "unity.h"
 #include "kstring.h"
 
+#include <string.h>
+#include <stdint.h>
+
+#include <stdio.h>
+
 uint16_t buffer[2048];
 
 void setUp(void) {
     terminal_initialize_test(&buffer[0]); //so that printing doesn't seg fault
 }
 void tearDown(void) {}
+
+
+void kint_to_string_util(const char *const expected_text, const int32_t number) {
+    char str[128u];
+    kint_to_string(number, str, 128u);
+    const size_t size_of_str = strlen(expected_text);
+    TEST_ASSERT_EQUAL_MEMORY(expected_text, str, size_of_str);
+}
 
 
 void test_cstrlen(void) {
@@ -33,11 +46,22 @@ void test_cstrcmp(void) {
     TEST_ASSERT_EQUAL_INT(0, kstrcmp(NULL, NULL));
 }
 
+void test_kint_to_string(void) {
+    kint_to_string_util("-12735", -12735);
+    kint_to_string_util("784032", 784032);
+    kint_to_string_util("0", 0);
+
+    //negative zero should be the same as positive zero since this is integers.
+    //contrast this with floats where 0.0f != -0.0f
+    kint_to_string_util("0", -0);
+}
+
 
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_cstrlen);
     RUN_TEST(test_cstrcmp);
+    RUN_TEST(test_kint_to_string);
     UNITY_END();
     return 0;
 }
--- test/Test-vga_driver.c
+++ test/Test-vga_driver.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-
 
 #define VGA_BUFFER_SIZE 80*25
 
