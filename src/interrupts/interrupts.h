79d5de55cddfa1f01f1000b577ea44d4b1020475
LincolnSand <sandlincoln@gmail.com>
2021-03-05 10:28:49 -0800

    Now using integer typedefs for IDT_entry

--- src/interrupts/interrupts.h
+++ src/interrupts/interrupts.h
@@ -9,11 +9,11 @@
 void idt_init(void);
 
 struct IDT_entry{
-    unsigned short int offset_lowerbits;
-    unsigned short int selector;
-    unsigned char zero;
-    unsigned char type_attr;
-    unsigned short int offset_higherbits;
+    uint16_t offset_lowerbits;
+    uint16_t selector;
+    uint8_t zero;
+    uint8_t type_attr;
+    uint16_t offset_higherbits;
 } __attribute__((packed));
 
 void irq0_handler(void);
