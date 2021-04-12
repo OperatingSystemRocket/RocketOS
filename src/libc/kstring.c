e3585ca03e7b5d30a5c4f5e4d02c015f3dde6791
lolshoc <lolshoc@gmail.com>
2021-04-12 09:47:51 -0700

    implemented majority of kstring.h, committing now to avoid terrible merge conflicts

--- src/libc/kstring.c
+++ src/libc/kstring.c
@@ -1,12 +1,122 @@
 #include "kstring.h"
 
-size_t kstrlen(const char *const str) {
-    kassert(str != NULL, 0);
+void* kmemcpy(void* destination, const void* source, size_t num) {
+    kassert(destination != NULL && source != NULL, 0);
 
     size_t len = 0;
-    while (str[len])
+    while(len < num) {
+        *((char*)destination + len) = *((char*)source + len);
         len++;
-    return len;
+    }
+    return destination;
+}
+
+void* kmemmove(void* destination, const void* source, size_t num) {
+    kassert(destination != NULL && source != NULL, 0);
+
+    char temp[num];
+    size_t len = 0;
+    while(len < num) {
+        *(temp + len) = *((char*)source + len);
+        len++;
+    }
+    len = 0;
+    while(len < num) {
+        *((char*)destination + len) = *(temp + len);
+        len++;
+    }
+    return destination;
+}
+
+void* kmemchr(void* ptr, int value, size_t num) {
+    kassert(ptr != NULL, 0);
+
+    size_t len = 0;
+    while(len < num) {
+        if(*((char*)ptr + len) = value) return ptr + len;
+        len++;
+    }
+    return NULL;
+}
+
+int kmemcmp(const void* ptr1, const void* ptr2, size_t num) {
+    kassert(ptr1 != NULL && ptr2 != NULL, 0);
+
+    size_t len = 0;
+    while(len < num) {
+        if(*((unsigned char*)ptr1 + len) < *((unsigned char*)ptr2 + len)) return -1;
+        if(*((unsigned char*)ptr1 + len) > *((unsigned char*)ptr2 + len)) return 1;
+        len++;
+    }
+    return 0;
+}
+
+void* kmemset(void *const ptr, const int32_t value, size_t num) {
+    char *const ptr_char = ptr; //so we can set each byte
+    const int8_t value_byte = value & 0xFFu; //this is to only grab the lowest byte and discard the rest as we are writing to chars
+
+    while(num--) {
+        *ptr_char = value_byte;
+    }
+
+    return ptr;
+}
+
+char* kstrcat(char *const destination, const char* source) {
+    kassert(destination != NULL && source != NULL, 0);
+    
+    char* ptr = destination + kstrlen(destination);
+    while(*source != '\0') {
+        *ptr = *source;
+        ptr++;
+        source++;
+    }
+
+    *ptr = '\0';
+
+    return destination;
+}
+
+char* kstrncat(char *const destination, const char* source, size_t num) {
+    kassert(destination != NULL && source != NULL, 0);
+    
+    char* ptr = destination + kstrlen(destination);
+    while(*source != '\0' && num > 0) {
+        *ptr = *source;
+        ptr++;
+        source++;
+        num--;
+    }
+
+    *ptr = '\0';
+
+    return destination;
+}
+
+char* kstrchr(char* str, int character) {
+    kassert(str != NULL, 0);
+    
+    if((char)character == '\0') return str + kstrlen(str);
+
+    size_t len = kstrlen(str);
+    size_t index = 0;
+    while(index < len) {
+        if(str[index] == (char)character) return str + index;
+        index++;
+    }
+    return NULL;
+}
+
+char* kstrrchr(char* str, int character) {
+    kassert(str != NULL, 0);
+    
+    if((char)character == '\0') return str + kstrlen(str);
+
+    size_t len = kstrlen(str);
+    while(len--) {
+        if(str[len] == (char)character) return str + len;
+    }
+    return NULL;
 }
 
 int32_t kstrcmp(const char *const lhs, const char *const rhs) {
@@ -41,19 +151,85 @@ int32_t kstrncmp(const char *const lhs, const char *const rhs, const size_t sz) 
 	return 0;
 }
 
-char* kstrcat(char *const destination, const char* source) {
+int kstrcoll(const char* str1, const char* str2) {
+    //TODO
+}
+
+char* kstrcpy(char* destination, const char* source) {
     kassert(destination != NULL && source != NULL, 0);
-    
-    char* ptr = destination + kstrlen(destination);
-    while(*source != '\0') {
-        *ptr = *source;
-        ptr++;
-        source++;
+
+    size_t len = 0;
+    while(source[len]) {
+       destination[len] = source[len];
+       len++;
     }
-
-    *ptr = '\0';
-
+    destination[len] = '\0';
     return destination;
+}
+
+char* kstrncpy(char* destination, const char* source, size_t num) {
+    kassert(destination != NULL && source != NULL, 0);
+
+    size_t len = 0;
+    size_t size = kstrlen(source);
+    char* temp = destination;
+    while(len < num) {
+        if(len < size) {
+            *(temp++) = source[len];
+        } else {
+            *(temp++) = '\0';
+        }
+        len++;
+    }
+    return destination;
+}
+
+char* kstrerror(int errnum) {
+    //TODO
+}
+
+size_t kstrspn(const char* str1, const char* str2) {
+    size_t cont = 0;
+    for(size_t i = 0; i < kstrlen(str1); i++) {
+        cont = 0;
+        for(size_t j = 0; j < kstrlen(str2); j++) {
+            if(str1[i] == str2[j]) {
+                cont = 1;
+                break;
+            }
+        }
+        if(!cont) return i;
+    }
+    return kstrlen(str1);
+}
+
+size_t kstrcspn(const char* str1, const char* str2) {
+    //TODO
+}
+
+const char* kstrpbrk(const char* str1, const char* str2) {
+    //TODO
+}
+
+const char* kstrstr(const char* str1, const char* str2) {
+    //TODO
+}
+
+char* kstrtok(char* str, const char* delimiters) {
+    //TODO
+}
+
+size_t kstrxfrm(char* destination, const char* source, size_t num) {
+    //TODO
+}
+
+size_t kstrlen(const char *const str) {
+    kassert(str != NULL, 0);
+
+    size_t len = 0;
+    while (str[len])
+        len++;
+    return len;
 }
 
 char* kint_to_string(int64_t input, char *const string_ret, const size_t ret_size) {
@@ -83,13 +259,3 @@ char* kint_to_string(int64_t input, char *const string_ret, const size_t ret_siz
     return string_ret;
 }
 
-void* kmemset(void *const ptr, const int32_t value, size_t num) {
-    char *const ptr_char = ptr; //so we can set each byte
-    const int8_t value_byte = value & 0xFFu; //this is to only grab the lowest byte and discard the rest as we are writing to chars
-
-    while(num--) {
-        *ptr_char = value_byte;
-    }
-
-    return ptr;
-}
