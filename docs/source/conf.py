c775072190ae0822841ea806b506d35bad2eb778
LincolnSand <sandlincoln@gmail.com>
2021-08-17 20:47:15 -0700

    Fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -18,8 +19,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'RocketOS'
-copyright = '2021, LincolnSand'
-author = 'LincolnSand'
+copyright = '2021, Lincoln S, Dexter W, Alan E'
+author = 'Lincoln S, Dexter W, Alan E'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
@@ -31,6 +32,10 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,12 +46,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+#html_theme = 'sphinx_rtd_theme'
 html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
